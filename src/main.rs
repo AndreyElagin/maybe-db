c990cbbbee1cbad68cd31ceefbe7ecba723c07b2
Andrey Elagin <daddyingrave@gmail.com>
2020-05-25 02:12:02 +0300

    part 3 wip

--- src/main.rs
+++ src/main.rs
@@ -37,11 +37,30 @@ fn main() {
 }
 
 use std::io::Write;
+use std::fmt::Formatter;
 
 fn print_prompt() {
     print!("db > ");
     let _ = io::stdout().flush();
 }
+
+const COLUMN_USERNAME_SIZE: u32 = 32;
+const COLUMN_EMAIL_SIZE: u32 = 255;
+
+const ID_SIZE: u32 = 4;
+const USERNAME_SIZE: u32 = 32;
+
+const EMAIL_SIZE: u32 = 255;
+
+const ID_OFFSET: u32 = 0;
+const USERNAME_OFFSET: u32 = ID_OFFSET + ID_SIZE;
+const EMAIL_OFFSET: u32 = USERNAME_OFFSET + USERNAME_SIZE;
+const ROW_SIZE: u32 = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+const PAGE_SIZE: u32 = 4096;
+const TABLE_MAX_PAGES: u32 = 100;
+const ROWS_PER_PAGE: u32 = PAGE_SIZE / ROW_SIZE;
+const TABLE_MAX_ROWS: u32 = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 enum MetaCommand {
     Success,
@@ -55,11 +74,42 @@ enum StatementType {
 
 enum PrepareResult {
     Success,
+    SyntaxError,
     Unrecognized(String),
 }
 
 struct Statement {
-    tp: StatementType
+    tp: StatementType,
+    // row_to_insert: Row
+}
+
+struct Row {
+    id: u32,
+    username: [char; COLUMN_USERNAME_SIZE as usize],
+    email: [char; COLUMN_EMAIL_SIZE as usize],
+}
+
+use std::fmt;
+
+impl fmt::Display for Row {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut uname = String::from("");
+        for i in 0usize..USERNAME_SIZE as usize {
+            if self.username[i].is_alphanumeric() {
+                todo!("doesn't work");
+                uname.push(self.username[i])
+            }
+        }
+        let mut email = String::from("");
+        for i in 0usize..EMAIL_SIZE as usize {
+            if self.username[i].is_alphanumeric() {
+                todo!("doesn't work");
+                email.push(self.email[i])
+            }
+        }
+
+        write!(f, "Row id: {}, username: {}, email: {}", self.id, uname, email)
+    }
 }
 
 fn do_meta_command(input: &str) -> MetaCommand {
